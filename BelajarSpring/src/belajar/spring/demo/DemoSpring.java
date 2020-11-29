e113bc926ba95290152466943ce675023b71ac79
kalitrack18 <kalitrack354@gmail.com>
2020-11-30 00:00:54 +0800

    Java APP yang menggunakan Spring

--- BelajarSpring/src/belajar/spring/demo/DemoSpring.java
+++ BelajarSpring/src/belajar/spring/demo/DemoSpring.java
@@ -1,8 +1,20 @@
 package belajar.spring.demo;
 
 
+import belajar.spring.domain.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class DemoSpring {
     public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("ClassPathResource:ioc.xml");
+
+        Person p = (Person) ctx.getBean("jimi");
+
+        //keluarkan data
+        System.out.println("Nama \t: "+ p.getNama());
+        System.out.println("Alamat \t: "+ p.getAlamat().getJalan());
+
 
     }
 }
