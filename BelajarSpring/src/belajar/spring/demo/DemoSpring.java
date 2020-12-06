3a860476a7c3cc588ba6fc5f444bb41194fe3430
kalitrack18 <kalitrack354@gmail.com>
2020-12-06 08:35:06 +0800

    Dasar Spring

--- BelajarSpring/src/belajar/spring/demo/DemoSpring.java
+++ BelajarSpring/src/belajar/spring/demo/DemoSpring.java
@@ -7,9 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class DemoSpring {
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:ioc.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("context.xml");
 
-        Person p = (Person) ctx.getBean("jimi");
+        Person p = (Person) ctx.getBean("message");
 
         //keluarkan data
         System.out.println("Nama \t: "+ p.getNama());
